449e73cea19659273d17b40ed86ec883d206346d
Fa-biano <fbiano.souza@gmail.com>
2023-07-26 16:38:27 -0300

    chore: install dotenv

--- database/config/config.js
+++ database/config/config.js
@@ -1,18 +1,4 @@
-// const config = {
-//   host: process.env.MYSQL_HOST || 'localhost',
-//   port: process.env.MYSQL_PORT || '3306',
-//   database: process.env.MYSQL_DB_NAME || 'aws-node-app',
-//   username: process.env.MYSQL_USER || 'root',
-//   password: process.env.MYSQL_PASSWORD || 'password',
-//   dialect: 'mysql',
-//   dialectOptions: {
-//     timezone: 'Z',
-//   },
-//   logging: false,
-// };
-
-
-// module.exports = config;
+require('dotenv').config();
 
 const environment = process.env.NODE_ENV || 'test';
 
